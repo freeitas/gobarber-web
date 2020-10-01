f52feb5680e183b1773c1f9edcc1de90b3815757
Guilherme Freitas <hfdsguilherme@gmail.com>
2020-10-01 08:03:44 -0300

    feat: Reset password page

--- src/pages/ForgotPassword/index.tsx
+++ src/pages/ForgotPassword/index.tsx
@@ -1,9 +1,9 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useState } from 'react';
 import { FiLogIn, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidationErros';
@@ -16,6 +16,7 @@ import Button from '../../components/Button';
 import {
   Container, Content, AnimationContainer, Background,
 } from './styles';
+import api from '../../services/api';
 
 interface ForgotPasswordFormData {
   email: string;
@@ -23,13 +24,15 @@ interface ForgotPasswordFormData {
 }
 
 const ForgotPssword: React.FC = () => {
+  const [loading, setLoading] = useState(false);
   const formRef = useRef<FormHandles>(null);
 
   const { addToast } = useToast();
-  const history = useHistory();
 
   const handleSubmit = useCallback(async (data: ForgotPasswordFormData) => {
     try {
+      setLoading(true);
+
       formRef.current?.setErrors({});
 
       const schema = Yup.object().shape({
@@ -40,7 +43,15 @@ const ForgotPssword: React.FC = () => {
         abortEarly: false,
       });
 
-      history.push('/dashboard');
+      await api.post('/password/forget', {
+        email: data.email,
+      });
+
+      addToast({
+        type: 'success',
+        title: 'E-mail de recuperação enviado',
+        description: 'Cheque sua caixa de entrada',
+      });
     } catch (err) {
       if (err instanceof Yup.ValidationError) {
         const errors = getValidationErrors(err);
@@ -55,6 +66,8 @@ const ForgotPssword: React.FC = () => {
         title: 'Erro na recuperação de senha',
         description: 'Ocorreu um erro ao tentar realizar a recuperação de senha, tente novamente.',
       });
+    } finally {
+      setLoading(false);
     }
   }, [addToast]);
 
